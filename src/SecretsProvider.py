e93dee4b98aa002f471d68893bb0cf4b542f063f
KoenGreuell <koengreuell@gmail.com>
2025-01-28 16:17:28 +0100

    #278 Use dotenv

--- src/SecretsProvider.py
+++ src/SecretsProvider.py
@@ -1,52 +1,54 @@
-import os
-import pprint
+import dotenv
 
 class SecretsProvider:
-    def __init__(self, environment_secret_prefix: str, input_func=input):
-        self.environment_secret_prefix = environment_secret_prefix
+    env_path = ".env"
+
+    def __init__(self, input_func=input):
+        """Saves and retrieves secrets
+        input_func controls how input from the user is asked when creating a new secret. By default, the user is asked for input.
+        """
         self.get_input = input_func
 
     def set_secret(self, secret_name: str):
-        '''Saves a secret in environment variables from user input.
+        """Saves a secret.
         If there is already a secret with the name given. Entering a new value will overwrite the existing secret.
-        '''
-        current_secret_value: str = os.environ.get(self.environment_secret_prefix + secret_name)
+        """
+        current_secret_value: str = dotenv.get_key(self.env_path, secret_name)
         if (current_secret_value):
-            print(f"\nThere is already a secret named {secret_name} stored as {self.environment_secret_prefix + secret_name}. Entering a new value will overwrite the existing secret.")
+            print(f"\nThere is already a secret named {secret_name} stored as {secret_name}. Entering a new value will overwrite the existing secret.")
         new_secret_value = self._request_secret_value()
         self._store_secret(secret_name, new_secret_value)
 
     def get_secret(self, secret_name: str) -> str:
-        '''Retrieves secret from environment variables
+        """Retrieves secret from environment variables
         looks for an environment variable environment_secret_prefix + secret_name
         if this secret does not exist, it prompts the user to enter a secret and stores that to environment variables.
-        '''
-        secret_value = os.environ.get(self.environment_secret_prefix + secret_name)
+        """
+        secret_value = dotenv.get_key(self.env_path, secret_name)
         if not secret_value:
-            print(f"\nA secret with name {self.environment_secret_prefix + secret_name} is not yet stored in environment variables.")
+            print(f"\nA secret with name {secret_name} is not yet stored in environment variables.")
             secret_value = self._request_secret_value()
             self._store_secret(secret_name, secret_value)
         return secret_value
 
     def remove_secret(self, secret_name: str) -> None:
-        '''Remove the secret from environment variables'''
-        os.environ.pop(self.environment_secret_prefix + secret_name)
+        """Remove the secret from environment variables"""
+        dotenv.unset_key(self.env_path, secret_name)
 
     def print_secrets(self) -> None:
-        '''Prints secrets in environment variables which have the environment_secret_prefix'''
-        secrets_from_environment_variables = {key: value for key, value in os.environ.items() if key.startswith(self.environment_secret_prefix)}
+        """Prints secrets"""
+        secrets_from_environment_variables = dotenv.dotenv_values(self.env_path)
         if secrets_from_environment_variables:
-            print(f"\nThere are {len(secrets_from_environment_variables)} secret(s) in environment variables using the prefix {self.environment_secret_prefix}")
-            pprint.pprint(secrets_from_environment_variables, width=1)
+            print(f"\nThere are {len(secrets_from_environment_variables)} secret(s) in environment variables: {secrets_from_environment_variables}")
         else:
-            print(f"\nSecrets using prefix {self.environment_secret_prefix} do not exist in environment variables.")
+            print(f"\nNothing is stored in the file {self.env_path}")
 
     def _request_secret_value(self) -> str:
         return self.get_input("Enter your Secret: ")
 
     def _store_secret(self, secret_name: str, secret_value: str) -> None:
         if secret_value.strip():
-            os.environ[self.environment_secret_prefix + secret_name] = secret_value
-            print(f"\nSecret has been stored as environment variable: {self.environment_secret_prefix + secret_name}.")
+            dotenv.set_key(self.env_path, secret_name, secret_value)
+            print(f"\nSecret has been stored as environment variable: {secret_name}.")
         else:
             print("\nNo value entered for the secret. Not storing.")
--- tests/test_secrets_provider.py
+++ tests/test_secrets_provider.py
@@ -1,95 +1,84 @@
 import os
+from dotenv import get_key
+from dotenv import set_key
 from unittest import TestCase
 from src.SecretsProvider import SecretsProvider
 
 class TestSecretsProvider(TestCase):
-    environment_secret_prefix = "Virtual_lab_user_secret_"
+    def tearDown(self):
+        if os.path.exists(SecretsProvider.env_path):
+            os.remove(SecretsProvider.env_path)
 
     def test_set_secret_new_secret(self):
         secret1_name: str = "secret1"
         secret1_value: str = "secret1_value"
         mock_input = lambda prompt: secret1_value
 
-        SecretsProvider(self.environment_secret_prefix, input_func=mock_input).set_secret(secret1_name)
+        SecretsProvider(input_func=mock_input).set_secret(secret1_name)
 
-        self.assertEqual(secret1_value, os.environ[self.environment_secret_prefix + secret1_name])
-
-        self.cleanup_environment_variable(secret1_name)
+        self.assertEqual(secret1_value, get_key(SecretsProvider.env_path, secret1_name))
 
     def test_set_secret_no_input(self):
         secret1_name: str = "secret1"
         mock_input = lambda prompt: ""
 
-        SecretsProvider(self.environment_secret_prefix, input_func=mock_input).set_secret(secret1_name)
+        SecretsProvider(input_func=mock_input).set_secret(secret1_name)
 
-        self.assertIsNone(os.environ.get(self.environment_secret_prefix + secret1_name))
+        self.assertIsNone(get_key(SecretsProvider.env_path, secret1_name))
 
     def test_set_secret_space_input(self):
         secret1_name: str = "secret1"
         mock_input = lambda prompt: " "
 
-        SecretsProvider(self.environment_secret_prefix, input_func=mock_input).set_secret(secret1_name)
+        SecretsProvider(input_func=mock_input).set_secret(secret1_name)
 
-        self.assertIsNone(os.environ.get(self.environment_secret_prefix + secret1_name))
+        self.assertIsNone(get_key(SecretsProvider.env_path, secret1_name))
 
     def test_set_secret_overwrite(self):
         secret1_name: str = "secret1"
         secret1_original_value: str = "secret1_original_value"
         secret1_new_value: str = "secret1_new_value"
-        os.environ[self.environment_secret_prefix + secret1_name] = secret1_original_value
+        set_key(SecretsProvider.env_path, secret1_name, secret1_original_value)
         mock_input = lambda prompt: secret1_new_value
 
-        SecretsProvider(self.environment_secret_prefix, input_func=mock_input).set_secret(secret1_name)
+        SecretsProvider(input_func=mock_input).set_secret(secret1_name)
 
-        self.assertEqual(secret1_new_value, os.environ.get(self.environment_secret_prefix + secret1_name))
-
-        self.cleanup_environment_variable(secret1_name)
+        self.assertEqual(secret1_new_value, get_key(SecretsProvider.env_path, secret1_name))
 
     def test_get_secret_nonexistent(self):
         secret1_name: str = "secret1"
         secret1_value: str = "secret1_value"
         mock_input = lambda prompt: secret1_value
 
-        secret_value = SecretsProvider(self.environment_secret_prefix, input_func=mock_input).get_secret(secret1_name)
+        secret_value = SecretsProvider(input_func=mock_input).get_secret(secret1_name)
 
-        self.assertEqual(secret1_value, os.environ[self.environment_secret_prefix + secret1_name], secret_value)
-
-        self.cleanup_environment_variable(secret1_name)
+        self.assertEqual(secret1_value, get_key(SecretsProvider.env_path, secret1_name), secret_value)
 
     def test_get_secret_existent(self):
         secret1_name: str = "secret1"
         secret1_value: str = "secret1_value"
-        os.environ[self.environment_secret_prefix + secret1_name] = secret1_value
+        set_key(SecretsProvider.env_path, secret1_name, secret1_value)
 
-        self.assertEqual(secret1_value, SecretsProvider(self.environment_secret_prefix).get_secret(secret1_name))
-
-        self.cleanup_environment_variable(secret1_name)
+        self.assertEqual(secret1_value, SecretsProvider().get_secret(secret1_name))
 
     def test_print_secrets(self):
         secret1_name: str = "secret1"
         secret1_value: str = "secret1_value"
         secret2_name: str = "secret2"
         secret2_value: str = "secret2_value"
-        os.environ[self.environment_secret_prefix + secret1_name] = secret1_value
-        os.environ[self.environment_secret_prefix + secret2_name] = secret2_value
+        set_key(SecretsProvider.env_path, secret1_name, secret1_value)
+        set_key(SecretsProvider.env_path, secret2_name, secret2_value)
 
-        SecretsProvider(self.environment_secret_prefix).print_secrets()
-
-        self.cleanup_environment_variable(secret1_name)
-        self.cleanup_environment_variable(secret2_name)
+        SecretsProvider().print_secrets()
 
     def test_print_secrets_no_secrets_stored(self):
-        SecretsProvider(self.environment_secret_prefix).print_secrets()
+        SecretsProvider().print_secrets()
 
     def test_remove_secret(self):
         secret1_name: str = "secret1"
         secret1_value: str = "secret1_value"
-        mock_input = lambda prompt: secret1_value
-        os.environ[self.environment_secret_prefix + secret1_name] = secret1_value
+        set_key(SecretsProvider.env_path, secret1_name, secret1_value)
 
-        SecretsProvider(self.environment_secret_prefix).remove_secret(secret1_name)
+        SecretsProvider().remove_secret(secret1_name)
 
-        self.assertIsNone(os.environ.get(self.environment_secret_prefix + secret1_name))
-
-    def cleanup_environment_variable(self, secret_name: str) -> None:
-        SecretsProvider(self.environment_secret_prefix).remove_secret(secret_name)
+        self.assertIsNone(get_key(SecretsProvider.env_path, secret1_name))
