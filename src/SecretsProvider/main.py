1f67b664058c93b937def73a9baa001abcc08658
KoenGreuell <koengreuell@gmail.com>
2025-01-29 09:58:40 +0100

    #278 Adapt to PEP 8 guidelines

--- src/SecretsProvider/main.py
+++ src/SecretsProvider/main.py
@@ -1,9 +1,11 @@
 import dotenv
 
+
 class SecretsProvider:
     def __init__(self, input_func=input, env_path=".env"):
         """Saves and retrieves secrets
-        input_func controls how input from the user is asked when creating a new secret. By default, the user is asked for input.
+        input_func controls how input from the user is asked when creating a new secret.
+        By default, the user is asked for input.
         """
         self.get_input = input_func
         self.env_path = env_path
@@ -14,7 +16,8 @@ class SecretsProvider:
         """
         current_secret_value: str = dotenv.get_key(self.env_path, secret_name)
         if (current_secret_value):
-            print(f"\nThere is already a secret named {secret_name} stored as {secret_name}. Entering a new value will overwrite the existing secret.")
+            print(f"\nThere is already a secret named {secret_name} stored as {secret_name}."
+                  f"Entering a new value will overwrite the existing secret.")
         new_secret_value = self._request_secret_value()
         self._store_secret(secret_name, new_secret_value)
 
